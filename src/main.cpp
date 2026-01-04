1249ff43c359d329645d2f3c0adce5ed4783d70b
Finlay Stewart <finlayjensonstewart@gmail.com>
2026-01-04 11:48:06 +0000

    Integrity check

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,37 @@
 #define SOL_ALL_SAFETIES_ON 1
 #include "sol/sol.hpp"
 
-// just ensures that all required variables exist
-int check_core_integrity(sol::state &lua) {
+// all errors in a namespace
+namespace errors {
+    
+    // node based errors
+    namespace node {
+
+        // loading errors
+        enum class load {
+            OK,
+            FILE,
+            INTEGRITY
+        };
+
+    };
+
+};
+
+int check_integrity(sol::state &lua) {
+    // check if there is an "array" (table) for availible nodes
+    sol::optional<sol::table> avail = lua["AVAILIBLE_NODES"];
+    
+    // if the template does not exist that fail the integrity test
+    if ( !avail ) {
+        return 1;
+    }
+
     return 0;
 }
 
-int main() {
-    // create lua interpreter
-    sol::state lua;
-
-    // open libs so we have access to print
-    lua.open_libraries(sol::lib::base, sol::lib::io, sol::lib::table);
-
+// gets all of the node types and verifies the integrity
+errors::node::load get_nodes(sol::state &lua, std::vector<std::string> &node_types) {
     // try to get the lua file
     try {
         lua.safe_script_file("core/node_data.lua");
@@ -22,14 +41,30 @@ int main() {
     }
     catch (const sol::error &e) {
         std::cout << e.what() << std::endl;
+        return errors::node::load::FILE;
+    }
+
+    if ( check_integrity(lua) == 1 ) {
+        return errors::node::load::INTEGRITY;
+    }
+
+    return errors::node::load::OK;
+}
+
+int main() {
+    // vector of strings (allows for expansion)
+    std::vector<std::string> node_types;
+
+    // create lua interpreter
+    sol::state lua;
+
+    // open libs so we have access to print
+    lua.open_libraries(sol::lib::base, sol::lib::io, sol::lib::table);
+
+    if ( get_nodes( lua, node_types ) != errors::node::load::OK ) {
+        std::cout << "An error has occurred when loading. Aborting..." << std::endl;
         return 1;
     }
-    
-    sol::table a = lua["AVAILIBLE_NODES"];
-
-    sol::table b = a[1];
-
-    std::cout << std::string(b["name"]) << std::endl;
 
     return 0;
 }
